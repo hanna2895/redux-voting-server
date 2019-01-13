ab9873237f9fa7331f450a1be7c44155fdb998f2
Hannah Werman <hanna.w28@gmail.com>
2019-01-12 20:21:55 -0600

    redux/node server

---
+++ index.js
@@ -0,0 +1,10 @@
+import makeStore from './src/store';
+import startServer from './src/server';
+
+export const store = makeStore();
+
+store.dispatch({
+    type: 'SET_ENTRIES',
+    entries: require('./entries.json')
+});
+startServer(store);
